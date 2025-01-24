1a43cefbb3bb15a9df19b88211098d324c84e71f
Pasidu Pahasara <pahasarapasidu@gmail.com>
2025-01-24 13:05:09 +0530

    Add LED control functions and update main loop for LED toggling

---
+++ STM32F405RGT6-BareMetal/Separate Checks/led_test/Inc/led.h
@@ -0,0 +1,14 @@
+/*
+ * led.h
+ *
+ *  Created on: Jan 24, 2025
+ *      Author: last_knight
+ */
+
+#ifndef LED_H_
+#define LED_H_
+
+void turn_on_led(int led);
+void turn_off_led(int led);
+
+#endif /* LED_H_ */
--- STM32F405RGT6-BareMetal/Separate Checks/led_test/Src/main.c
+++ STM32F405RGT6-BareMetal/Separate Checks/led_test/Src/main.c
@@ -1,2 +1,53 @@
-#include "stm32f4xx.h"       //Include the STM32F4xx header file(Standard peripheral library)
-#include "stm32f405xx.h"
+#include <stdio.h>//Include the standard input/output header file
+#include <stdint.h> //Include the standard integer header file
+#include "stm32f4xx.h" //Include the STM32F4xx header file(Standard peripheral library)
+#include "stm32f405xx.h"//Include the STM32F411 header file(Standard peripheral library)
+#include "led.h" //Include the led header file
+
+
+int main(void)
+{
+    while (1)
+    {
+        turn_on_led(9);
+        for(volatile int i = 0; i < 100000; i++);
+        turn_off_led(9);
+        for(volatile int i = 0; i < 100000; i++);
+        turn_on_led(6);
+        for(volatile int i = 0; i < 100000; i++);
+        turn_off_led(6);
+        for(volatile int i = 0; i < 100000; i++);
+        turn_on_led(5);
+        for(volatile int i = 0; i < 100000; i++);
+        turn_off_led(5);
+        for(volatile int i = 0; i < 100000; i++);
+        turn_on_led(4);
+        for(volatile int i = 0; i < 100000; i++);
+        turn_off_led(4);
+        for(volatile int i = 0; i < 100000; i++);
+        turn_on_led(11);
+        for(volatile int i = 0; i < 100000; i++);
+        turn_off_led(11);
+        for(volatile int i = 0; i < 100000; i++);
+        turn_on_led(10);
+        for(volatile int i = 0; i < 100000; i++);
+        turn_off_led(10);
+        for(volatile int i = 0; i < 100000; i++);
+        turn_on_led(1);
+        for(volatile int i = 0; i < 100000; i++);
+        turn_off_led(1);
+        for(volatile int i = 0; i < 100000; i++);
+        turn_on_led(2);
+        for(volatile int i = 0; i < 100000; i++);
+        turn_off_led(2);
+        for(volatile int i = 0; i < 100000; i++);
+        turn_on_led(3);
+        for(volatile int i = 0; i < 100000; i++);
+        turn_off_led(3);
+        for(volatile int i = 0; i < 100000; i++);
+
+
+
+    }
+    
+}
