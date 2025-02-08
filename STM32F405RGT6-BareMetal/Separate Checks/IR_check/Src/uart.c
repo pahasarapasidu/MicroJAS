a82f80f719749d1d51293cc9fc851241d11eef8d
Pasidu Pahasara <pahasarapasidu@gmail.com>
2025-02-08 20:13:39 +0530

    Implement UART baud rate configuration functions in uart.c

--- STM32F405RGT6-BareMetal/Separate Checks/IR_check/Src/uart.c
+++ STM32F405RGT6-BareMetal/Separate Checks/IR_check/Src/uart.c
@@ -1,2 +1,21 @@
 #include "uart.h"
 
+
+
+#define SYS_FREQ    16000000U
+#define APB1_CLK    SYS_FREQ
+
+#define UART_BAUDRATE 115200U
+
+static void uart_set_baudrate(USART_TypeDef *USARTx, uint32_t PeriphClk, uint32_t BaudRate)
+{
+    /****Write Calculated Baudrate to any USARTx BRR****/
+  USARTx->BRR = compute_uart_bd(PeriphClk, BaudRate);
+}
+
+static uint16_t compute_uart_bd(uint32_t PeriphClk, uint32_t BaudRate)
+{
+    /****USART BaudRate devident factor(N) Calculation
+                      SYS_CLK/BR = N                  ****/
+  return ((PeriphClk + (BaudRate/2U))/BaudRate);
+}
