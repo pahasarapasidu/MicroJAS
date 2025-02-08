e5a74d7455a09022f8161aecc40532f8268623d4
Pasidu Pahasara <pahasarapasidu@gmail.com>
2025-02-08 20:54:06 +0530

    Add functions to configure GPIO pins for UART TX and RX modes in uart.c

--- STM32F405RGT6-BareMetal/Separate Checks/IR_check/Src/uart.c
+++ STM32F405RGT6-BareMetal/Separate Checks/IR_check/Src/uart.c
@@ -1,11 +1,26 @@
 #include "uart.h"
 
-
+#define GPIOCEN     (1UL << 2)
+#define USART3EN    (1UL << 18)
 
 #define SYS_FREQ    16000000U
 #define APB1_CLK    SYS_FREQ
 
 #define UART_BAUDRATE 115200U
+
+void pc10_af_uart_tx_mode(void){
+    /*Enable GPIOC clock*/
+    /*Set PC10 to alternate function mode*/
+    /*Set PC10 alternate function type to UART_TX (USART3)*/
+
+
+}
+
+void pc11_af_uart_rx_mode(void){
+    /*Enable GPIOC clock*/
+    /*Set PC11 to alternate function mode*/
+    /*Set PC11 alternate function type to UART_RX (USART3)*/
+}
 
 static void uart_set_baudrate(USART_TypeDef *USARTx, uint32_t PeriphClk, uint32_t BaudRate)
 {
